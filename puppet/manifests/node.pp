2f59e2e8b23cc1fe5ece31ea234b12dee97d1b48
Edwin Biemond <biemond@gmail.com>
2014-07-24 22:43:25 +0200

    removed swap

--- puppet/manifests/node.pp
+++ puppet/manifests/node.pp
@@ -9,7 +9,7 @@ node 'node1.example.com', 'node2.example.com' {
   
   include os, ssh, java, orawls::weblogic, orautils, bsu, copydomain, nodemanager
 
-  Class['java'] -> Class['orawls::weblogic'] 
+  Class['java'] -> Class['orawls::weblogic']
 }
 
 # operating settings for Middleware
@@ -19,24 +19,24 @@ class os {
   $host_instances = hiera('hosts', {})
   create_resources('host',$host_instances, $default_params)
 
-  exec { "create swap file":
-    command => "/bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=8192",
-    creates => "/var/swap.1",
-  }
+  # exec { "create swap file":
+  #   command => "/bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=8192",
+  #   creates => "/var/swap.1",
+  # }
 
-  exec { "attach swap file":
-    command => "/sbin/mkswap /var/swap.1 && /sbin/swapon /var/swap.1",
-    require => Exec["create swap file"],
-    unless => "/sbin/swapon -s | grep /var/swap.1",
-  }
+  # exec { "attach swap file":
+  #   command => "/sbin/mkswap /var/swap.1 && /sbin/swapon /var/swap.1",
+  #   require => Exec["create swap file"],
+  #   unless => "/sbin/swapon -s | grep /var/swap.1",
+  # }
 
-  #add swap file entry to fstab
-  exec {"add swapfile entry to fstab":
-    command => "/bin/echo >>/etc/fstab /var/swap.1 swap swap defaults 0 0",
-    require => Exec["attach swap file"],
-    user => root,
-    unless => "/bin/grep '^/var/swap.1' /etc/fstab 2>/dev/null",
-  }
+  # #add swap file entry to fstab
+  # exec {"add swapfile entry to fstab":
+  #   command => "/bin/echo >>/etc/fstab /var/swap.1 swap swap defaults 0 0",
+  #   require => Exec["attach swap file"],
+  #   user => root,
+  #   unless => "/bin/grep '^/var/swap.1' /etc/fstab 2>/dev/null",
+  # }
 
   service { iptables:
     enable    => false,
